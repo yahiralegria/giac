83657bb83679290cec524590c24769a0c222403b
alegria1026 <yahirhumberto04@gmail.com>
2026-06-27 00:54:25 -0600

    Update favicon assets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,9 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="icon" href="/favicon.ico?v=3" sizes="any" />
+                <link rel="icon" href="/favicon.svg?v=3" type="image/svg+xml" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3" />
             </Head>
             <Header />
             <Component {...pageProps} />
